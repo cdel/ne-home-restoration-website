9688d1c857cd0f1d8d4db059287c9c4a1722911a
Javier Guerrero <steven.guesp@gmail.com>
2018-03-25 19:40:10 -0400

    Create source data for application state

--- src/data/state.js
+++ src/data/state.js
@@ -1,33 +1,101 @@
 const state = {
-  todos: [
-    {
-      title: "Wash dishes",
-      id: 1234,
-      description: "I need to wash dishes by 10pm!",
-      date: "03/23/18",
-      completed: false
+  social_media: {
+    facebook: {
+      username: "newenglandhomerestoration",
+      url: "https://www.facebook.com/New-England-Home-Restoration-1116943771718268/"
     },
-    {
-      title: "Finish project for Brian",
-      id: 1235,
-      description: "I want to finish taht shit now!",
-      date: "03/23/18",
-      completed: false
-    },
-    {
-      title: "Learn Redux",
-      id: 1236,
-      description: "I need to wash dishes by 10pm!",
-      date: "03/23/18",
-      completed: false
-    },
-    {
-      title: "retire early",
-      id: 1237,
-      description: "I need to wash dishes by 10pm!",
-      date: "03/23/18",
-      completed: false
+    instagram: {
+      username: "newenglandhomerestoration",
+      url: "https://www.instagram.com/newenglandhomerestoration/"
     }
-  ]
+  },
+  about: {
+    owner: {
+      name: "Andre LeBlanc",
+      background: "The owner, Andre LeBlanc, began developing his passion for carpentry and construction at age 14. He was employed every summer during high school for a licensed builder, honing his skill set in carpentry. He went on to receive a degree in Construction Management. After graduating college, Andre acquired his real estate license and spent a few years working in the industry so that he could develop a full understanding of the residential housing market.\n He enjoyed helping individuals with the process of purchasing or selling their homes. He knew he would be working in construction, and he continued to build the knowledge and skills to succeed from all angles. For several years, he worked as a lead carpenter for a company that focused on timber framing and historical restoration. In New England, a large percentage of homes, are built pre-1900, and this niche specialty is an integral part of what makes Andre’s background unique. Growing up in Vermont, on a family-run farm, Andre naturally wanted to get involved and understand how older houses and barns were constructed at that time. He enjoys working with reclaimed lumber and timber framed houses or barns when the opportunity arises. Andre has combined his unique experience, passion, and artistry together as one to form New England Home Restoration."
+    },
+    company: {
+      name: "New England Home Restoration",
+      background: "Whether an entire home renovation, a kitchen or bath remodel, or adding a beautiful new addition to your residence, our clients choose New England Home Restoration for our expert craftsmanship and unrivaled customer service. Integrity is at our core, and we respect both our client’s time and expectations. We offer a truly custom approach, where we take the time to carefully assess our client's goals, and provide experienced feedback. Our team always prioritizes building trust, which is why most of our clients stay with us for a lifetime.\n New England Home Restoration combines a deep commitment to customer intimacy and collaboration throughout all phases of the relationship. With a background in hands-on carpentry and years of experience in construction management, owner Andre LeBlanc prides himself and his team on quality craftsmanship and is an expert in understanding the many steps required to complete a project."
+    }
+  },
+  contact: {
+    phone_number: "781-800-0121",
+    email: "Office@NewEnglandHomeRestoration.com",
+    areas_served: ["Acton", "Arlington", "Bedford", "Belmont", "Bolton", "Boston", "Boxborough", "Brookline", "Burlington", "Brookline", "Cambridge", "Dedham", "Framingham", "Harvard", "Hudson", "Lexington", "Littleton", "Lynnfield", "Malden", "Malborough", "Maynard", "Medford", "Melrose", "Milton", "Nahant", "Natick", "Needham", "Newton", "Norfolk", "Northborough", "North Reading", "Norton", "Norwood", "Reading", "Salem", "Somerville", "Stoneham", "Stow", "Wakefield", "Waltham", "Watertown", "Westborough", "Wellesley", "Weymouth", "Wilmington", "Winchester", "Woburn"],
+    licenses: [
+      {
+       name: "Home Improvement Contractor (MA)",
+       license_id: 185354,
+      },
+      {
+        name: "Massachusetts Dept of Public Safety License",
+        license_id: "CSFA-106295"
+      }
+    ],
+    accreditation: [
+      {
+        name: "NARI Accredited",
+        url: "https://www.nari.org/arc/"
+      },
+      {
+        name: "Better Business Bureau",
+        url: "https://www.bbb.org/council/for-businesses/about-bbb-accreditation/"
+      }
+    ],
+    insurances: ["Mass. Workesr' Compensation", "General Liability Insurance"],
+    payments: ["Cash", "Credit/Debit", "Check", "Financing"]
+  },
+  project_management: {
+    description: "At New England Home Restoration, our online system gives you easy access to everything about your project. You can make selections, see progress, browse photos, review financial decisions, send messages, and more. All from any computer, tablet, or smartphone.",
+    benefits: [],
+    video_src: "https://player.vimeo.com/video/207894066?title=0&byline=0&portrait=0"
+  },
+  gallery: {
+    title: "Showcasing our work",
+    kitchens: [],
+    basements: [],
+    bathrooms: []
+  },
+  financing: {
+    title: "Financing Available",
+    description: "We offer financing through GreenSky!"
+  },
+  faq: {
+    title: "New England Home Restoration’s Most Frequently Asked Questions",
+    description: "At New England Home Restoration. We take the time to explain the details about the services you request. We answer all of your questions politely and explain our estimates for your home improvement project to your satisfaction. We believe an educated client is our best customer so we’ve provided this page of Common Questions for your benefit.",
+    faqs: [
+      {
+        question: "Why are some contractors so much less expensive than others?",
+        answer: "Not all contractors are created equal. Many people pose as \”contractors” who are actually unlicensed, uninsured, and/or use illegal labor. In addition, these \"contractors\" tend to use subpar materials and poor craftsmanship. If you are hiring a contractor, check their license, their insurance, and call their references (we are more than happy to supply these to you). Protect yourself. Hire a licensed, bonded, and insured contractor.\n More often than not, you get what you pay for. A good contractor provides quality input when you meet to discuss your plans. The good ones will help you realize your dreams. The bad ones will merely take down your ideas, itemize the list, add prices and ask for your signature.. and your cash. What you want is the contractor who does a thorough inspection, demonstrates knowledge of what is needed and what the options are. Never assume that all contractors know the latest building technologies, products and code requirements. You want the industry leader not the follower. At New England Home Restoration we pride ourselves on staying up to date on new products, technologies, and techniques.",
+        id: 123
+      },
+      {
+        question: "How much will my home improvement cost?",
+        answer: "Projects vary widely depending on your needs and budget. New England Home Restoration’s owner, Andre LeBlanc, will visit with you to better understand your home improvement needs. At New England Home Restorations we do more than give you a quote. We provide you with the information you need to make the right decisions for your home. We take time to explain the details regarding your project’s design options and cost. We answer all of your questions and explain the estimates for your home improvement project. There is no obligation.",
+        id: 124
+      },
+      {
+        question: "Will there be a big mess during our project?",
+        answer: "We respect your property by using all means to keep your home as clean and dust free as possible; these include drop cloths, furniture covers, ram boards, and BuildClean HEPA air scrubber. The BuildClean unit eliminates 90% of airborne dust and the air that it does clean is expelled outside and is not re-circulated.",
+        id: 125
+      },
+      {
+        question: "How long will my project take?",
+        answer: "Once we have your order in hand, we begin production planning. Andre and the designers involved will first schedule a visit to your home to confirm the measurements and quantities needed for ordering materials. Work specifications for each trade skill are broken down into separate work orders. Purchase orders are prepared and sent to each vendor supplying the materials. Once the work is started on your home, it is our company’s policy to proceed to completion. It is important to each homeowner to understand that our concern is much the same as yours; the successful completion of your project in a timely and clean manner.",
+        id: 126
+      },
+      {
+        question: "When will the project be completed?",
+        answer: "Our company provides clients with a schedule before construction begins which includes a project completion deadline. Changes made by the Owner, unforeseen circumstances (such as weather delays or concealed conditions) may add time to the overall schedule; however, the schedule will be updated and redistributed when a delay occurs so that all parties are aware of the changes. The project is completed with the final walk through with Andre at which time the final payment is expected.",
+        id: 127
+      },
+      {
+        question: "What products do you like to use the most in kitchens and bathrooms?",
+        answer: "At New England Home Restorations, we really enjoy using Wedi. It is a family owned company that offers waterproof and directly tileable building panels. It is lightweight, easy to work with and best of all, creates zero dust, unlike other products available. Andre is a certified Wedi installer and takes pleasure in the opportunity to discuss how Wedi products can be used in kitchens and bathrooms.",
+        id: 128
+      }
+    ]
+  }
 };
 export default state;
