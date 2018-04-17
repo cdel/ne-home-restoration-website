25a5dcd7b4b0f385b7ea5e19c41a3f4e66d51c76
Javier Guerrero <steven.guesp@gmail.com>
2018-04-17 05:09:14 -0400

    Add footer markup and styles

--- src/client/js/react/components/Footer/Footer.jsx
+++ src/client/js/react/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import {Grid, Row, Col} from 'react-bootstrap';
 import './footer.scss';
 
 const Footer = props => {
@@ -56,103 +57,93 @@ const Footer = props => {
   return (
   <footer className="Footer page-footer font-small blue-grey lighten-5 pt-0">
     <div className="Footer-socialMediaBar">
-      <div className="row py-4 d-flex align-items-center">
-          <div className="col-12 col-md-5 text-left mb-4 mb-md-0">
+      <Grid> 
+        <Row>
+          <Col xs={12} md={5}>
             <h6 className="mb-0 white-text text-center text-md-left">
                 <strong>Get connected with us on social networks!</strong>
             </h6>
-          </div>
-          <div className="col-12 col-md-7 text-center text-md-right">
+          </Col>
+          <Col xs={12} md={7}>
             <a className="fb-ic ml-0">
-            <i className="fa fa-facebook white-text mr-lg-4"> </i>
+              <i className="fa fa-facebook white-text mr-lg-4"> </i>
+              </a>
+              <a className="tw-ic">
+              <i className="fa fa-twitter white-text mr-lg-4"> </i>
+              </a>
+              <a className="gplus-ic">
+              <i className="fa fa-google-plus white-text mr-lg-4"> </i>
+              </a>
+              <a className="li-ic">
+              <i className="fa fa-linkedin white-text mr-lg-4"> </i>
+              </a>
+              <a className="ins-ic">
+              <i className="fa fa-instagram white-text mr-lg-4"> </i>
             </a>
-            <a className="tw-ic">
-            <i className="fa fa-twitter white-text mr-lg-4"> </i>
-            </a>
-            <a className="gplus-ic">
-            <i className="fa fa-google-plus white-text mr-lg-4"> </i>
-            </a>
-            <a className="li-ic">
-            <i className="fa fa-linkedin white-text mr-lg-4"> </i>
-            </a>
-            <a className="ins-ic">
-            <i className="fa fa-instagram white-text mr-lg-4"> </i>
-            </a>
-          </div>
-      </div>
+          </Col>
+        </Row>
+      </Grid>
     </div>
-    <div className="container mt-5 mb-4 text-center text-md-left">
-        <div className="row mt-3">
-          <div className="col-md-3 col-lg-4 col-xl-3 mb-4 dark-grey-text">
-              <h6 className="text-uppercase font-weight-bold">
-                <strong>Company name</strong>
-              </h6>
-              <hr className="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" data-style="width: 60px;" />
-              <p>Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit
-                amet, consectetur adipisicing elit.
-              </p>
-          </div>
-          <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 dark-grey-text">
-              <h6 className="text-uppercase font-weight-bold">
-                <strong>Products</strong>
-              </h6>
-              <hr className="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" data-style="width: 60px;" />
-              <p>
-                <a href="#!" className="dark-grey-text">MDBootstrap</a>
-              </p>
-              <p>
-                <a href="#!" className="dark-grey-text">MDWordPress</a>
-              </p>
-              <p>
-                <a href="#!" className="dark-grey-text">BrandFlow</a>
-              </p>
-              <p>
-                <a href="#!" className="dark-grey-text">Bootstrap Angular</a>
-              </p>
-          </div>
-          <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4 dark-grey-text">
-              <h6 className="text-uppercase font-weight-bold">
-                <strong>Useful links</strong>
-              </h6>
-              <hr className="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" data-style="width: 60px;" />
-              <p>
-                <a href="#!" className="dark-grey-text">Your Account</a>
-              </p>
-              <p>
-                <a href="#!" className="dark-grey-text">Become an Affiliate</a>
-              </p>
-              <p>
-                <a href="#!" className="dark-grey-text">Shipping Rates</a>
-              </p>
-              <p>
-                <a href="#!" className="dark-grey-text">Help</a>
-              </p>
-          </div>
-          <div className="col-md-4 col-lg-3 col-xl-3 dark-grey-text">
-              <h6 className="text-uppercase font-weight-bold">
-                <strong>Contact</strong>
-              </h6>
-              <hr className="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" data-style="width: 60px;" />
-              <p>
-                <i className="fa fa-home mr-3"></i> New York, NY 10012, US
-              </p>
-              <p>
-                <i className="fa fa-envelope mr-3"></i> info@example.com
-              </p>
-              <p>
-                <i className="fa fa-phone mr-3"></i> + 01 234 567 88
-              </p>
-              <p>
-                <i className="fa fa-print mr-3"></i> + 01 234 567 89
-              </p>
-          </div>
-        </div>
+    <div className="Footer-content">
+      <Grid> 
+        <Row>
+          <Col md={3} lg={4} xl={3}>
+            <h6 className="text-uppercase font-weight-bold">
+              <strong>Company name</strong>
+            </h6>
+            <hr className="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" data-style="width: 60px;" />
+            <p>Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit
+              amet, consectetur adipisicing elit.
+            </p>
+          </Col>
+          <Col md={2} lg={2} xl={2}>
+            <h6 className="text-uppercase font-weight-bold">
+              <strong>Products</strong>
+            </h6>
+            <hr className="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" data-style="width: 60px;" />
+            <p>
+              <a href="#!" className="dark-grey-text">MDBootstrap</a>
+            </p>
+            <p>
+              <a href="#!" className="dark-grey-text">MDWordPress</a>
+            </p>
+            <p>
+              <a href="#!" className="dark-grey-text">BrandFlow</a>
+            </p>
+            <p>
+              <a href="#!" className="dark-grey-text">Bootstrap Angular</a>
+            </p>
+          </Col>
+          <Col md={4} lg={3} xl={3}>
+            <h6 className="text-uppercase font-weight-bold">
+              <strong>Contact</strong>
+            </h6>
+            <hr className="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" data-style="width: 60px;" />
+            <p>
+              <i className="fa fa-home mr-3"></i> New York, NY 10012, US
+            </p>
+            <p>
+              <i className="fa fa-envelope mr-3"></i> info@example.com
+            </p>
+            <p>
+              <i className="fa fa-phone mr-3"></i> + 01 234 567 88
+            </p>
+            <p>
+              <i className="fa fa-print mr-3"></i> + 01 234 567 89
+            </p>
+          </Col>      
+        </Row>
+      </Grid>
     </div>
     <div className="Footer-attribution">
-        © 2018 Copyright:
-        <a href="https://mdbootstrap.com/material-design-for-bootstrap/">
-        <strong> MDBootstrap.com</strong>
-        </a>
+      <Grid>
+        <Col xs={12}>
+          © 2018 Copyright:
+          <a href="https://mdbootstrap.com/material-design-for-bootstrap/">
+          <strong> MDBootstrap.com</strong>
+          </a>
+        </Col>
+      </Grid>
     </div>
   </footer>
   );
