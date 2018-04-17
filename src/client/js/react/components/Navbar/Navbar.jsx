ede8bdadf77c29a5323cae99ebed0068064b0759
Javier Guerrero <steven.guesp@gmail.com>
2018-04-17 03:45:25 -0400

    Make header stick

--- src/client/js/react/components/Navbar/Navbar.jsx
+++ src/client/js/react/components/Navbar/Navbar.jsx
@@ -5,22 +5,58 @@ import { Image } from 'react-bootstrap';
 import cx from '_utilities/classnames';
 import './Navbar.scss';
 
+const OFFSET = 20;
+
 class NavigationBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isExpanded: false
+      isExpanded: false,
+      isStickying: false,
+      height: null
     };
     this.toggleMenu = () => {
       this.setState(currState => ({
         isExpanded: !currState.isExpanded
       }));
     }
+    this.getHeaderRef = ref => {
+      this.headerRef = ref;
+      this.setState({
+        height: this.headerRef.clientHeight
+      });
+    }
+    this.getHeaderContentRef = ref => {
+      this.headerContentRef = ref;
+    }
+    this.handleScroll = (e) => {
+      const isStickying = window.scrollY >= this.headerRef.clientHeight;
+      if (this.state.isStickying !== isStickying) {
+        this.setState({
+          isStickying
+        });
+      }
+    };
   }
+  shouldComponentUpdate(nextState) {
+    const didUpdate = Object.keys(nextState).some(state => {
+      return nextState[state] !== this.state[state];
+    });
+    return didUpdate;
+  }
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
   render() {
     const props = this.props;
+    const {isExpanded, height, isStickying} = this.state;
+    const styles = isStickying ? {height} : {};
     return (
-      <header className="Navbar">
+      <header ref={this.getHeaderRef} className="Navbar" style={styles}>
         <div className="Navbar-banner">
           <p className="Navar-banner-message">
             Call Us Today At: <a className="Navbar-banner-tel" href={`tel:${props.phoneNumber}`}>{props.phoneNumber}</a>
@@ -34,8 +70,9 @@ class NavigationBar extends React.Component {
             ))}
           </ul>
         </div>
-        <div className={cx('Navbar-header', {
-            'Navbar-header--expanded': this.state.isExpanded
+        <div ref={this.getHeaderContentRef} className={cx('Navbar-header', {
+            'Navbar-header--expanded': isExpanded,
+            'Navbar-header--isStickying': isStickying
           })}>
           <figure className="Navbar-header-logo-wrapper">
             <img className="Navbar-header-logo-image" src="assets/logo/NEHomeRest_logo.png" alt="New England Home Restoration"/>
@@ -43,8 +80,8 @@ class NavigationBar extends React.Component {
           <nav className="Navbar-header-navigation" onClick={this.toggleMenu}>
             <button className="Navbar-header-navigation-toggle">
               <span className={cx('Navbar-header-navigation-toggle-icon', {
-                'glyphicon glyphicon-remove': this.state.isExpanded,
-                'glyphicon glyphicon-align-justify': !this.state.isExpanded
+                'glyphicon glyphicon-remove': isExpanded,
+                'glyphicon glyphicon-align-justify': !isExpanded
               })}></span>
             </button>
             <ul className="Navbar-header-navigation-menu">
